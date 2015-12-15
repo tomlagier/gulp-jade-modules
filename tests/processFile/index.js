cfa485ea4b5e97310616c52196b7dd01912be647
Tom Lagier <tom@rosetta.com>
2015-12-14 16:14:22 -0800

    processFile tests

---
+++ tests/processFile/index.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var JadePathWriter = require('../../jade-path-writer.js');
+var chai = require('chai');
+var path = require('path');
+var vinyl = require('vinyl');
+var vinylFile = require('vinyl-file');
+
+chai.should();
+
+module.exports = describe('#processFile', function(){
+  var jpw = new JadePathWriter();
+  var entry = vinylFile.readSync(path.resolve(__dirname + '/start.jade'));
+
+  var processedFiles = jpw.processFile(entry, [path.resolve(__dirname)]);
+  it('should return an array', function() {
+    processedFiles.should.be.a('array');
+  });
+
+  it('should return each included file', function() {
+    processedFiles.should.have.length(3);
+  });
+
+});
